2c6ccaecdaffaeb9bd1462c043155ceb1d1e2c8f
AsyaFet <aanastai.logos@gmail.com>
2020-02-29 18:18:04 +0200

    js2part

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,18 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-console.log('webpack starterkit');
+const user = {
+  name: "Asya",
+  lastName: "Fetisova",
+  age: 19,
+  birth: "31.12.2000",
+  height: 178,
+  animal: true,
+  foodPrefer: ["chicken", "juice", "cucumber"],
+
+  getInform: function() {
+    console.log(this.name + this.lastName + this.age);
+  }
+};
+
+console.log(user);
+user.getInform();
